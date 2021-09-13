c1c73c6fe578fef627d26228e25271908ba0f5cb
Powerminer11 <linuscarlberg@hotmail.se>
2021-09-13 20:25:02 +0200

    Feature 3.1, create reviews skeleton.

---
+++ src/project/review/CreateReviews.java
@@ -0,0 +1,40 @@
+package project.review;
+
+
+import java.util.Scanner;
+
+public class CreateReviews {
+
+    public CreateReviews() {
+
+    }
+
+    public void createReview() {
+        int CreatedID = 3;
+        //just making an ID for the program to run. Feature 2.1 Should make the ID.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the ID of the item you wish to review");
+        int ID = scanner.nextInt();
+
+        if (ID == CreatedID) {
+            System.out.println("Please give a score from 1-5");
+
+            int Score = scanner.nextInt();
+
+            if (Score > 0 && Score < 6) {
+                System.out.println("You have given " + ID + " " + Score + " stars");
+                System.out.println("Your item review was registered successfully."
+                );
+
+            } else {
+                System.out.println("Score must be between 1 and 5");
+            }
+        } else {
+
+        System.out.println("Item "+ ID + " was not registered yet");
+
+        }
+    }
+}
